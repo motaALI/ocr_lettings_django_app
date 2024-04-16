793bcec92a8b6e24dd3157a6091ddaff1887e7a8
motaALI <motawakil.ali.90@gmail.com>
2024-04-16 19:46:24 +0200

    [ADD] test_settings

---
+++ oc_lettings_site/test_settings.py
@@ -0,0 +1,9 @@
+# test_settings.py
+
+# Import settings from main settings file
+from settings import *
+
+# Remove Whitenoise middleware from MIDDLEWARE list
+MIDDLEWARE = [
+    mw for mw in MIDDLEWARE if mw != 'whitenoise.middleware.WhiteNoiseMiddleware'
+]
