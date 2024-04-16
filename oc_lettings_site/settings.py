6c3c3d4b682d3f159a001af8ee0ed5dd202c4a2f
motaALI <motawakil.ali.90@gmail.com>
2024-04-16 20:19:39 +0200

    [ADD] STATICFILES_STORAGE

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -72,12 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
