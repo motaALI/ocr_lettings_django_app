e6b93cd7a8818c2468792a3e4925479c52fa05d0
motaALI <motawakil.ali.90@gmail.com>
2024-04-17 09:36:12 +0200

    [ADD] Strdoc on views

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render
 
 def custom_403(request, exception):
+    """
+    Renders a custom 403 error page.
+    Args:
+        request: The HTTP request object.
+        exception: The exception that triggered the 403 error.
+
+    Returns: A rendered 403 error page.
+    """
     return render(request, '403.html', status=403)
 
 def custom_404(request, exception):
+    """
+    Renders a custom 404 error page.
+
+    Args:
+        request: The HTTP request object.
+        exception: The exception that triggered the 404 error.
+
+    Returns: A rendered 404 error page.
+    """
     return render(request, '404.html', status=404)
 
 def custom_500(request):
+    """
+    Renders a custom 500 error page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns: A rendered 500 error page.
+    """
     return render(request, '500.html', status=500)
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
+
+# Home  
 def index(request):
+    """
+    Renders the home page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered home page.
+    """
     return render(request, 'index.html')
 
